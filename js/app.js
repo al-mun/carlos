7e286350c2403be567cee48bb2df79f86d1d9482
Alejandro <63469503+alejandro-mun@users.noreply.github.com>
2021-06-12 15:25:38 -0400

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -19,5 +19,25 @@ closeBtn.addEventListener("click", ()=> {
   sidebar.classList.remove("show-sidebar");
 });
 
+const addButtons=()=>{
+  const addBtns = document.querySelector(".add-buttons")
+  //const modifyButtons = document.querySelector(".modify-buttons")
+  const deleteButtons = document.querySelector(".delete-buttons")
+  if(addBtns.style.display === "flex"){
+    addBtns.style.display = "none"
+    //modifyButtons.style.display = "none"
+    //deleteButtons.style.display = "none"
+  }
+  else{
+    //modifyButtons.style.display = "flex"
+    //deleteButtons.style.display = "flex"
+    addBtns.style.alignItems = "center"
+    addBtns.style.justifyContent = "center"
+    addBtns.style.display = "flex"
+  }
+}
+
+
+
 // set year
 date.innerHTML = new Date().getFullYear();
